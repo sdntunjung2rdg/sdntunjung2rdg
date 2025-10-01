07d8cfe27fced4d647fc31b850382515a22a9e3b
sdntunjung2rdg <sdntunjung02randuagung@gmail.com>
2025-10-01 13:01:29 +0700

    Add files via upload

--- page/index.js
+++ page/index.js
@@ -1,1 +1,13 @@
+import Navbar from "../components/Navbar";
 
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="container mt-5">
+        <h1 className="text-center">Selamat Datang di SDN Tunjung 02 Raduagung</h1>
+        <p className="text-center">Mencetak generasi berprestasi dan berakhlak mulia.</p>
+      </div>
+    </>
+  );
+}
